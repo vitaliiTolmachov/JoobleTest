d663b241823225d8cbe177467e3f6360208009c1
Vitalii Tolmachov <v.tolmachov@gmail.com>
2019-07-10 14:02:08 +0300

    Working fine. Need to try load test words one by one

--- WordsBreaker/Program.cs
+++ WordsBreaker/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GenericParsing;
 
 namespace WordsBreaker
 {
@@ -10,6 +12,58 @@ namespace WordsBreaker
     {
         static void Main(string[] args)
         {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Content/de-test-words.tsv");
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"File with test words not found in folder: {filePath}\r\nPress any key to exit");
+                Console.ReadKey();
+                return;
+            }
+            var parser = new GenericParser(filePath, Encoding.UTF8)
+            {
+                SkipEmptyRows = true,
+                StripControlChars = true,
+                FirstRowHasHeader = true,
+                TextFieldType = FieldType.Delimited,
+                ColumnDelimiter = '\t'
+            };
+            int minWordLength = Int32.MaxValue;
+            List<string> testWords = new List<string>();
+            using (parser)
+            {
+                while (parser.Read())
+                {
+                    string word = parser[1];
+
+                    if (word.Length < minWordLength)
+                        minWordLength = word.Length;
+                    testWords.Add(word);
+                }
+            }
+            filePath = Path.Combine(Directory.GetCurrentDirectory(), "Content/dict");
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"Dictionary file not found in folder: {filePath}\r\nPress any key to exit");
+                Console.ReadKey();
+                return;
+            }
+            var wordsComparer = new WordsComparer();
+            var tempLookup = File.ReadAllLines(filePath)
+                .Where(line => !string.IsNullOrWhiteSpace(line))
+                .Select(s => s.ToLower())
+                .Distinct()
+                .ToList();
+            var r = new HashSet<string>(tempLookup);
+            
+
+            var wordBraker = new WordsBreaker(minWordLength);
+            wordBraker.FindWordConcatenationInLookup(testWords, r.ToDictionary(
+                keySelector: word => word,
+                elementSelector: word => word
+                ));
+            wordBraker.PrintResult();
+            Console.ReadKey();
+
         }
     }
 }
---
+++ WordsBreaker/WordsBreaker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsBreaker
+{
+    public class WordsBreaker
+    {
+        private readonly int _minWordLength;
+        private Dictionary<string, string> _lookup;
+        private List<string> _testwords;
+        private Dictionary<string, HashSet<string>> result = new Dictionary<string, HashSet<string>>();
+
+        public WordsBreaker(int minWordLength)
+        {
+            _minWordLength = minWordLength;
+        }
+        public void FindWordConcatenationInLookup(List<string> testWords, Dictionary<string, string> lookup)
+        {
+            _testwords = testWords;
+            _lookup = lookup;
+
+            for (int i = 0; i < testWords.Count; i++)
+            {
+                FindWord(testWords[i], i);
+            }
+        }
+
+        private void FindWord(string word, int index)
+        {
+            result[word] = new HashSet<string>();
+            int currentWordLength = word.Length;
+            for (var charIndex = _minWordLength; charIndex < currentWordLength; charIndex++)
+            {
+                var prefix = word.Substring(0, charIndex);
+                var suffix = word.Substring(charIndex);
+
+                //We are not interested in suffix wich is smaller than the smallest words
+                if (suffix.Length < _minWordLength)
+                    break;
+
+                if (_lookup.ContainsKey(prefix))
+                {
+                    if (_lookup.ContainsKey(suffix))
+                    {
+                        //If word = prefix + sufix it's contactenated word we are looking for
+                        result[word].Add(prefix);
+                        result[word].Add(suffix);
+                        break;
+                    }
+                    //Let's find another part to concatenate with prefix
+                    FindWord(suffix, index);
+                }
+            }
+        }
+
+        public void PrintResult()
+        {
+            foreach (KeyValuePair<string, HashSet<string>> entry in result.Where(pair => pair.Value.Any()))
+            {
+                Console.WriteLine($"{entry.Key} can be braked into parts:\r\n");
+                foreach (string part in entry.Value)
+                {
+                    Console.WriteLine($"\t{part}");
+                }
+            }
+        }
+    }
+}
